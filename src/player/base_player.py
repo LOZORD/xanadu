a3ca07a9c15cfe4db23ae98f296847ed1c17aef8
LOZORD <ljrudberg@gmail.com>
2015-10-16 22:11:36 -0500

    Updated BasePlayer to use kwargs

--- src/player/base_player.py
+++ src/player/base_player.py
@@ -3,18 +3,17 @@ from abilities import Abilities
 class BasePlayer(object, PlayerStats, Abilities):
   WESTERN_ALLEGIANCE = 1
   EASTERN_ALLEGIANCE = 2
-  def __init__(self, name, x, y, init_gold, modifiers, allegiance, abilities):
+  # def __init__(self, name, x, y, init_gold, modifiers, allegiance, abilities):
+  def __init__(self, **kwargs):
     self.uid = None # TODO
-    self.name = name
-    self.x = x
-    self.y = y
-    self.gold = init_gold
+    self.name = kwargs['name']
+    self.x = kwargs['x']
+    self.y = kwargs['y']
+    self.gold = kwargs['init_gold']
     self.inventory = []
     self.party = None
-    self.modifiers = modifiers
-    self.allegiance = allegiance
-    # self.requires_translation = True
-    # self.can_distinguish_poison = False
+    self.modifiers = kwargs['modifiers']
+    self.allegiance = kwargs['allegiance']
 
   def join_party(self, some_party):
     self.party = some_party
