12d3ff5f63b651f73700a9420b8541b82272843d
LOZORD <ljrudberg@gmail.com>
2015-10-16 22:23:16 -0500

    Added Doctor character

---
+++ src/player/doctor.py
@@ -0,0 +1,10 @@
+from base_player import BasePlayer
+
+class Doctor(Base_Player):
+  def __init__(self, **kwargs):
+    super(Doctor, self).__init__(kwargs)
+    self.inventory.extend(['morphine', 'opium', 'medical kits',
+      'poison antidote', 'e-w translation book', 'matches'])
+    self.craftables.extend(['poison', 'poison antidote'])
+    self.can_translate_modern = True
+    self.can_fillet = True
