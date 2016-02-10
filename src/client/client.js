548242b0c38cdd94019043572c14ea7670d777a1
LOZORD <ljrudberg@gmail.com>
2016-02-09 19:15:09 -0600

    Basic form on LHS implemented

--- src/client/client.js
+++ src/client/client.js
@@ -3,4 +3,15 @@ let socket = io();
 $(document).ready(() => {
   let parentContainer = $('#parent-container');
   parentContainer.append('socket and jquery are ready!');
+
+  let form = $('#main-form');
+  // TODO: style this
+  let input = $('#main-input');
+
+  form.submit((event) => {
+    event.preventDefault();
+    let msg = input.val();
+    input.val('');
+    console.log(msg);
+  });
 });
