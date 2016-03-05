8b9de99be65f0cfafe051b55e34d4a3042585c80
LOZORD <ljrudberg@gmail.com>
2016-03-04 23:46:50 -0600

    Supported better messaging for the client

--- src/client/client.js
+++ src/client/client.js
@@ -21,11 +21,41 @@ $(document).ready(() => {
     socket.emit('message', msg);
   };
 
-  let addMessage = (msg) => {
-    messageOutput.append($('<li>').text(msg));
+  let processData = (data) => {
+    var ret = {};
+    ret.message = data.message;
+    ret.classes = []; // in case we want to add multiple classes
+
+    if (data.type === 'echo') {
+      ret.classes.push('via-echo');
+    } else if (data.type === 'message') {
+      ret.classes.push('via-message');
+    } else if (data.type === 'whisper') {
+      ret.message = data.speaker + ' said: ' + ret.message;
+      ret.classes.push('via-whisper');
+    } else {
+      console.err('unknown message data type');
+    }
+
+    return ret;
+  };
+
+  let addMessage = (processedData) => {
+    var msg = processedData.message;
+    var classes = processedData.classes || [];
+    var newElem = $('<li>').text(msg);
+    if (processedData.classes) {
+      newElem.addClass(classes.join(' '));
+    }
+    messageOutput.append(newElem);
     messageOutput.parent().scrollTop(messageOutput.parent().height());
     input.focus();
   };
+
+  // Basic welcome message
+  addMessage({
+    message: 'Please enter your name below...'
+  });
 
   socket.on('rejected-from-room', () => {
     addMessage('GAME ROOM AT CAPACITY');
@@ -34,7 +64,9 @@ $(document).ready(() => {
 
   socket.on('message', (data) => {
     console.log('message', data);
-    addMessage(JSON.stringify(data));
+    var processedData = processData(data);
+    console.log(processedData);
+    addMessage(processedData);
   });
 
   // DEPRECATED
