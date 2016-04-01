000223029acb5e718540e7e0604b5e2ee8ebb0e2
LOZORD <ljrudberg@gmail.com>
2016-03-31 21:15:02 -0500

    Created 'map' directory

---
+++ src/server/map/cell.js
@@ -0,0 +1,12 @@
+// a Map is made by an NxN grid of Cells
+/* Things that extend Cell
+  - Room
+  - TreasureRoom
+  - EntranceRoom
+  - Wall (a solid rock that can be excavated into a room)
+*/
+class Cell {
+  constructor() {
+    // TODO
+  }
+}
