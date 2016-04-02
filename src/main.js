0a7160a538c9f066d9f665cc8cf665bbfe6394a9
Zach Thomae <zthomae@gmail.com>
2016-04-01 22:41:05 -0500

    Add basic command-line argument parser

--- src/main.js
+++ src/main.js
@@ -1,5 +1,52 @@
 let Game = require('./server/game');
+let process = require('process');
 
-let myGame = new Game();
+let die = function(msg) {
+  console.error(msg);
+  process.exit(1);
+};
+
+let parseArgs = function(argv) {
+  let args = {};
+  let i = 2; // skip node and filename
+  while (i < argv.length) {
+    if (argv[i] == '--port') {
+      let port = parseInt(argv[i+1]);
+      if (isNaN(port) || port < 1 || port > 65535) {
+        die(`Invalid port "${ argv[i+1] }"`);
+      } else {
+        args.port = port;
+      }
+      i += 2;
+    } else if (argv[i] == '--ns') {
+      // Not supported
+      die('Unsupported argument --ns');
+    } else if (argv[i] == '--maxPlayers') {
+      let maxPlayers = parseInt(argv[i+1]);
+      if (isNaN(maxPlayers) || maxPlayers < 2) {
+        die(`Invalid maxPlayers "${ argv[i+1] }"`);
+      } else {
+        args.maxPlayers = maxPlayers;
+      }
+    } else if (argv[i] == '--players') {
+      // Not supported
+      die('Unsupported argument --player');
+    } else if (argv[i] == '--seed') {
+      let seed = parseInt(argv[i+1]);
+      if (isNaN(seed)) {
+        die(`Invalid seed "${ argv[i+1] }"`);
+      } else {
+        args.seed = seed;
+      }
+      i += 2;
+    } else {
+      die(`Unexpected arg "${ argv[i] }"`);
+    }
+  }
+  return args;
+};
+
+let args = parseArgs(process.argv);
+let myGame = new Game(args);
 
 myGame.play();
