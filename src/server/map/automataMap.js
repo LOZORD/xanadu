bea39fb97e0c26794dcf56553d638eb0bde3e908
LOZORD <ljrudberg@gmail.com>
2016-04-07 16:40:41 -0500

    Filled out cellular automata generator

--- src/server/map/automataMap.js
+++ src/server/map/automataMap.js
@@ -2,7 +2,7 @@
  * http://www.roguebasin.com/index.php?title=Cellular_Automata_Method_for_Generating_Random_Cave-Like_Levels
  */
 let _   = require('lodash');
-// let Rng = require('random-seed');
+let Rng = require('random-seed');
 
 const CELL_TYPES = {
   ROOM: '_',
@@ -14,6 +14,7 @@ const CELL_TYPES = {
 class CellularAutomataMapGenerator {
   constructor(seed, dimension, percentWalls) {
     this.seed = seed;
+    this.rng  = new Rng(this.seed);
     this.dim  = dimension;
     this.percentWalls = percentWalls;
     this.map = [[]];
@@ -60,15 +61,52 @@ class CellularAutomataMapGenerator {
     });
   }
   placeWallLogic(x, y) {
-    // TODO
-    return [null, x, y];
+    let numWalls = adjacentWallCount(x, y, 1, 1);
+    if (this.getCell(x, y) == CELL_TYPES.BARRIER) {
+      if (numWalls >= 4) {
+        return CELL_TYPES.BARRIER;
+      } else if (numWalls < 2) {
+        return CELL_TYPES.ROOM;
+      }
+    } else {
+      if (numWalls > = 5 {
+        return CELL_TYPES.BARRIER;
+      }
+    }
+
+    return CELL_TYPES.ROOM;
   }
   randomFillMap() {
-    // TODO
-    return null;
+    //let newMap = [[]];
+
+    let mapMiddle = this.dim / 2;
+
+    _.range(this.dim, (y) => {
+      _.range(this.dim, (x) => {
+        if (x === 0) {
+          this.setCell(x, y, CELL_TYPES.BARRIER);
+        } else if (y === 0) {
+          this.setCell(x, y, CELL_TYPES.BARRIER);
+        } else if (x === this.dim - 1) {
+          this.setCell(x, y, CELL_TYPES.BARRIER);
+        } else if (y === this.dim - 1) {
+          this.setCell(x, y, CELL_TYPES.BARRIER);
+        } else {
+          if (row == mapMiddle) {
+            this.setCell(x, y, CELL_TYPES.ROOM);
+          } else {
+            this.setCell(x, y, this.randomPercent(this.percentWalls);
+          }
+        }
+      });
+    });
   }
   randomPercent(perc) {
-    return [null, perc];
+    if (perc >= this.rng.intBetween(1, 100)) {
+      return CELL_TYPES.BARRIER;
+    } else {
+      return CELL_TYPES.ROOM;
+    }
   }
   toString() {
     let cols = _.map(this.map, (col) => col.join(''));
