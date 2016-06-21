72187cea86b9be64e3f4554de76b6731fb768f4e
LOZORD <ljrudberg@gmail.com>
2016-06-21 17:52:44 -0500

    Added Istanbul for coverage

---
+++ src/client/clientSpec.js
@@ -0,0 +1,6 @@
+import { it, beforeEach } from 'arrow-mocha/es5';
+import { expect } from 'chai';
+
+describe('Client', () => {
+  it('should be tested!');
+});
---
+++ src/server/serverSpec.js
@@ -0,0 +1,6 @@
+import { it, beforeEach } from 'arrow-mocha/es5';
+import { expect } from 'chai';
+
+describe('Server', () => {
+  it('should be tested!');
+});
