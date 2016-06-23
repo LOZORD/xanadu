f9c4504ee101879fffa10f6d66b68ebdde7c52e8
LOZORD <ljrudberg@gmail.com>
2016-06-22 23:21:15 -0500

    Fixed naming issue in Cell

--- src/game/map/cell.js
+++ src/game/map/cell.js
@@ -14,48 +14,48 @@ export default class Cell {
   neighbors() {
     const x = this.x;
     const y = this.y;
-    let neighbors = {};
+    let myNeighbors = {};
 
     // NW
     if (this.map.hasCell(x-1, y-1)) {
-      neighbors.nw = this.map.getCell(x-1, y-1);
+      myNeighbors.nw = this.map.getCell(x-1, y-1);
     }
 
     // N
     if (this.map.hasCell(x, y-1)) {
-      neighbors.n = this.map.getCell(x, y-1);
+      myNeighbors.n = this.map.getCell(x, y-1);
     }
 
     // NE
     if (this.map.hasCell(x+1, y-1)) {
-      neighbors.ne = this.map.getCell(x+1, y-1);
+      myNeighbors.ne = this.map.getCell(x+1, y-1);
     }
 
     // W
     if (this.map.hasCell(x-1, y)) {
-      neighbors.w = this.map.getCell(x-1, y);
+      myNeighbors.w = this.map.getCell(x-1, y);
     }
 
     // E
     if (this.map.hasCell(x+1, y)) {
-      neighbors.e = this.map.getCell(x+1, y);
+      myNeighbors.e = this.map.getCell(x+1, y);
     }
 
     // SW
     if (this.map.hasCell(x-1, y+1)) {
-      neighbors.sw = this.map.getCell(x-1, y+1);
+      myNeighbors.sw = this.map.getCell(x-1, y+1);
     }
 
     // S
     if (this.map.hasCell(x, y+1)) {
-      neighbors.s = this.map.getCell(x, y+1);
+      myNeighbors.s = this.map.getCell(x, y+1);
     }
 
     // SE
     if (this.map.hasCell(x+1, y+1)) {
-      neighbors.se = this.map.getCell(x+1, y+1);
+      myNeighbors.se = this.map.getCell(x+1, y+1);
     }
 
-    return neighbors;
+    return myNeighbors;
   }
 }
