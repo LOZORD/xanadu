f514ed1df1156caf37b52fca71bf7f39b69447b3
LOZORD <ljrudberg@gmail.com>
2016-07-22 11:30:39 -0500

    Converted and rewrote Context tests (using Lobby)

--- src/context/context.ts
+++ src/context/context.ts
@@ -9,90 +9,98 @@ export const validName: RegExp = /^\w+$/;
 export type NameValidation = 'Valid' | 'Taken' | 'Invalid characters';
 
 export interface Command {
-    player: Player;
-    contents: string;
-    ts: number;
+  player: Player;
+  contents: string;
+  ts: number;
 }
 
 // A Context is a mutable container of Players. Other classes, like Game,
 // can own a Context that they use for dispatch.
 export abstract class Context {
-    players: Player[];
-    maxPlayers: number;
+  players: Player[];
+  maxPlayers: number;
 
-    constructor(maxPlayers: number, players?: Player[]) {
-        this.maxPlayers = maxPlayers;
-        this.players = players || [];
+  constructor(maxPlayers: number, players?: Player[]) {
+    this.maxPlayers = maxPlayers;
+    this.players = players || [];
+  }
+
+  getPlayer(id: string): Player {
+    return _.find(this.players, (player) => player.id === id);
+  }
+
+  getPlayerByName(name: string): Player {
+    return _.find(this.players, (player) => player.name === name);
+  }
+
+  hasPlayer(id: string): boolean {
+    return this.getPlayer(id) !== undefined;
+  }
+
+  hasPlayerByName(name: string): boolean {
+    return this.getPlayerByName(name) !== undefined;
+  }
+
+  addPlayer(id: string): void {
+    if (this.hasPlayer(id)) {
+      throw new Error(`Player with id ${id} already exists!`);
+    } else if (this.isAcceptingPlayers()) {
+      this.players.push({
+        id,
+        name: '', // This is odd
+        state: 'Anon'
+      });
+    } else {
+      throw new Error('Attempted to add a player when not accepting!');
+    }
+  }
+
+  removePlayer(id: string): Player {
+    const removedPlayer = this.getPlayer(id);
+    this.players = _.filter(this.players, p => p.id !== id);
+    return removedPlayer;
+  }
+
+  updatePlayer(id: string, update: { state?: PlayerState, name?: string }): void {
+    const player = _.find(this.players, p => p.id === id);
+
+    if (!player) {
+      throw new Error('Could not find player to update with id: ' + id);
     }
 
-    getPlayer(id: string): Player {
-        return _.find(this.players, (player) => player.id === id);
+    if (update.name) {
+      player.name = update.name;
     }
-
-    getPlayerByName(name: string): Player {
-        return _.find(this.players, (player) => player.name === name);
+    if (update.state) {
+      player.state = update.state;
     }
+  }
 
-    hasPlayer(id: string): boolean {
-        return this.getPlayer(id) !== undefined;
+  isAcceptingPlayers(): boolean {
+    return this.players.length < this.maxPlayers;
+  }
+
+  validateName(name: string): NameValidation {
+    // TODO: validate against "subset" names
+    // e.g. "James_Bond" and "James_Bond_007"
+    if (this.hasPlayerByName(name)) {
+      return 'Taken';
+    } else if (!validName.test(name)) {
+      return 'Invalid characters';
+    } else {
+      return 'Valid';
     }
+  }
 
-    hasPlayerByName(name: string): boolean {
-        return this.getPlayerByName(name) !== undefined;
-    }
+  broadcast(message: string): Message {
+    return createGameMessage(message, this.players);
+  }
 
-    addPlayer(id: string): void {
-        if (this.hasPlayer(id)) {
-            throw new Error(`Player with id ${id} already exists!`);
-        }
-        this.players.push({
-            id,
-            name: '', // This is odd
-            state: 'Anon'
-        });
-    }
+  // Signal to the server whether it is time to create a new context for the players
+  // (Example: everyone in a Lobby is ready, so start a game)
+  abstract isReadyForNextContext(): boolean;
 
-    removePlayer(id: string): Player {
-        const removedPlayer = this.getPlayer(id);
-        this.players = _.filter(this.players, p => p.id === id);
-        return removedPlayer;
-    }
-
-    updatePlayer(id: string, update: { state?: PlayerState, name?: string }): void {
-        const player = _.find(this.players, p => p.id === id);
-        if (update.name) {
-            player.name = update.name;
-        }
-        if (update.state) {
-            player.state = update.state;
-        }
-    }
-
-    isAcceptingPlayers(): boolean {
-        return this.players.length < this.maxPlayers;
-    }
-
-    validateName(name: string): NameValidation {
-        // TODO: validate against "subset" names
-        // e.g. "James_Bond" and "James_Bond_007"
-        if (this.hasPlayerByName(name)) {
-            return 'Taken';
-        } else if (!validName.test(name)) {
-            return 'Invalid characters';
-        } else {
-            return 'Valid';
-        }
-    }
-
-    broadcast(message: string): Message {
-        return createGameMessage(message, this.players);
-    }
-
-    // Signal to the server whether it is time to create a new context for the players
-    // (Example: everyone in a Lobby is ready, so start a game)
-    abstract isReadyForNextContext(): boolean;
-
-    abstract handleCommand(m: Command, p: Player): Message[];
+  abstract handleCommand(m: Command, p: Player): Message[];
 }
 
 export default Context;
---
+++ src/context/contextSpec.ts
@@ -0,0 +1,121 @@
+import { expect } from 'chai';
+import * as _ from 'lodash';
+
+import Context from './context';
+import Lobby from './lobby';
+import { Player } from '../game/player';
+import { createEchoMessage } from '../game/messaging';
+
+describe('Context (tested via Lobby)', () => {
+
+  // for the sake of clarity:
+  const testContext = context;
+
+  describe('addPlayer', () => {
+    beforeEach(function () {
+      this.context = new Lobby(2, []);
+    });
+    testContext('when there is room available', () => {
+      it('should add the player', function () {
+        this.context.addPlayer('007');
+        expect(this.context.hasPlayer('007')).to.be.true;
+      });
+    });
+    testContext('when the id is already present', () => {
+      it('should throw an error', function () {
+        const add007 = () => this.context.addPlayer('007');
+        // not present
+        expect(add007).not.to.throw(Error);
+        // present
+        expect(add007).to.throw(Error);
+      });
+    });
+    testContext('when the game is full', () => {
+      it('should throw an error', function () {
+        const ap = (id) => (() => this.context.addPlayer(id));
+        expect(ap('foo')).not.to.throw(Error);
+        expect(ap('bar')).not.to.throw(Error);
+        expect(ap('baz')).to.throw(Error);
+      });
+    });
+  });
+
+  describe('removePlayer', () => {
+    beforeEach(function() {
+      this.context = new Lobby(8, [{
+        name: 'James_Bond',
+        id: '007',
+        state: 'Ready'
+      }]);
+    });
+    testContext('when the player is present', () => {
+      it('should remove and return the player', function() {
+        const bond = this.context.removePlayer('007');
+        expect(bond.id).to.equal('007');
+        expect(this.context.hasPlayer('007')).to.be.false;
+        expect(this.context.players.length).to.equal(0);
+      });
+    });
+    testContext('when the player is NOT present', () => {
+      it('should return undefined', function() {
+        const noOne = this.context.removePlayer('foobar');
+        expect(noOne).to.be.undefined;
+      });
+    });
+  });
+
+  describe('broadcast', () => {
+    before(function() {
+      this.players =  [
+        { name: 'Alex', id: 'guitar', state: 'Ready' },
+        { name: 'Geddy', id: 'bass', state: 'Ready' },
+        { name: 'Neil', id: 'drums', state: 'Ready' }
+      ];
+
+      this.context = new Lobby(8, this.players);
+    });
+    it('should return a game message addressed to all players', function() {
+      const msg = this.context.broadcast('hell world');
+      expect(msg.to).to.eql(this.players);
+    });
+  });
+
+  describe('validateName', () => {
+    it('should be tested!');
+  });
+
+  describe('updatePlayer', () => {
+    testContext('when the player is present', () => {
+      before(function() {
+        this.originalPlayer = {
+          name: 'nada',
+          id: '007',
+          state: 'Preparing'
+        };
+
+        this.context = new Lobby(8, [_.cloneDeep(this.originalPlayer)]);
+
+        this.context.updatePlayer('007', {
+          name: 'James_Bond',
+          state: 'Ready'
+        });
+
+        this.updatedPlayer = this.context.getPlayerByName('James_Bond');
+      });
+      it('should update the name field', function() {
+        expect(this.updatedPlayer.name).to.equal('James_Bond');
+      });
+      it('should update the state field', function() {
+        expect(this.updatedPlayer.state).to.equal('Ready');
+      });
+    });
+
+    testContext('when the player is NOT present', () => {
+      it('should throw an error', () => {
+        expect(
+          () => ( (new Lobby(8, [])).updatePlayer('foobar', {}) )
+        ).to.throw(Error);
+      });
+    });
+  });
+});
