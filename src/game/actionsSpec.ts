2ee5220373901ffa6c4bb3d71825e6907766d1a9
LOZORD <ljrudberg@gmail.com>
2016-11-19 16:20:15 -0500

    Added AttackAction component
    Tested parsing and validation; still need to test performance!

--- src/game/actionsSpec.ts
+++ src/game/actionsSpec.ts
@@ -379,4 +379,238 @@ describe('Actions', function () {
       it('should throw an error if the item chosen is not ingestible');
     });
   });
+  describe('AttackAction', function () {
+    beforeEach(function () {
+      const p1 = Player.createPlayer('alice', 'Alice', 'Playing');
+      const p2 = Player.createPlayer('bob', 'Bob', 'Playing');
+
+      this.game = new Game(8, [ p1, p2 ]);
+
+      // let's give Alice some weapons!
+      this.p1 = (this.game as Game).getPlayer('alice');
+      this.p2 = (this.game as Game).getPlayer('bob');
+
+      (this.p1.character as Character.Character).inventory.maximumCapacity = 100;
+      const aliceInv1 = Inventory.addToInventory((this.p1.character as Character.Character).inventory, 'Knife', 1, 1);
+      const aliceInv2 = Inventory.addToInventory(aliceInv1, 'Rifle', 1, 1);
+      const aliceInv3 = Inventory.addToInventory(aliceInv2, 'Rifle Bullet', 10, 10);
+
+      // But take away her Revolver and Revolver Bullets, if she has any
+      let aliceInv4: Inventory.Inventory;
+      let aliceInv5: Inventory.Inventory;
+
+      if (Inventory.hasItem(aliceInv3, 'Revolver')) {
+        aliceInv4 = Inventory.removeFromInventory(aliceInv3, 'Revolver', Infinity).inventory;
+      } else {
+        aliceInv4 = aliceInv3;
+      }
+
+      if (Inventory.hasItem(aliceInv4, 'Revolver Bullet')) {
+        aliceInv5 = Inventory.removeFromInventory(aliceInv4, 'Revolver Bullet', Infinity).inventory;
+      } else {
+        aliceInv5 = aliceInv4;
+      }
+
+      (this.p1.character as Character.Character).inventory = aliceInv5;
+    });
+    describe('parse', function () {
+      context('when given a valid input', function () {
+        it('should return a correctly parsed attack action', function () {
+          const parse = Actions.ATTACK_COMPONENT.parse('attack bob knife 2', this.p1.character, Date.now());
+
+          expect(parse).to.be.ok;
+          expect(parse.key).to.eql('Attack');
+          expect(parse.targetName).to.eql('bob');
+          expect(parse.times).to.eql(2);
+          expect(parse.weaponName).to.eql('Knife');
+        });
+      });
+      context('when given an invalid input', function () {
+        it('should throw an error', function () {
+          expect(
+            () => Actions.ATTACK_COMPONENT.parse('foobarbaz123', this.p1.character, Date.now())
+          ).to.throw(Error);
+        });
+      });
+    });
+    describe('validate', function () {
+      context('when the item is not in the inventory', function () {
+        it('should not be valid', function () {
+          const missingItemAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            times: 1,
+            timestamp: Date.now(),
+            targetName: 'Bob',
+            weaponName: 'Revolver',
+            key: 'Attack'
+          };
+
+          const result = Actions.ATTACK_COMPONENT.validate(missingItemAction, this.game);
+
+          expect(result.isValid).to.be.false;
+          expect(result.error).to.contain('Missing Revolver in inventory!');
+        });
+      });
+      context('when the number of attack times is bad', function () {
+        it('should not be valid', function () {
+          const nanAttackAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            times: NaN,
+            timestamp: Date.now(),
+            targetName: 'Bob',
+            weaponName: 'Knife',
+            key: 'Attack'
+          };
+
+          const negativeAttackAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            times: -1,
+            timestamp: Date.now(),
+            targetName: 'Bob',
+            weaponName: 'Knife',
+            key: 'Attack'
+          };
+
+          [ nanAttackAction, negativeAttackAction ].forEach(action => {
+            const result = Actions.ATTACK_COMPONENT.validate(action, this.game);
+
+            expect(result.isValid).to.be.false;
+            expect(result.error).to.contain('Bad number of times attacking');
+          });
+        });
+      });
+      context('when the target DNE', function () {
+        it('should not be valid', function () {
+          const missingTargetAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            times: 1,
+            timestamp: Date.now(),
+            targetName: 'Carlo',
+            weaponName: 'Fist',
+            key: 'Attack'
+          };
+
+          const result = Actions.ATTACK_COMPONENT.validate(missingTargetAction, this.game);
+
+          expect(result.isValid).to.be.false;
+          expect(result.error).to.contain("No player with name 'Carlo'");
+        });
+      });
+      context('when the target is beyond the attack range', function () {
+        beforeEach(function () {
+          // move Bob south
+          this.p2.character.row += 1;
+        });
+        afterEach(function () {
+          // move Bob back to the starting position
+          this.p2.character.row -= 1;
+        });
+        it('should not be valid', function () {
+          const outOfRangeAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            timestamp: Date.now(),
+            times: 1,
+            targetName: 'bo',
+            weaponName: 'Knife',
+            key: 'Attack'
+          };
+
+          const result = Actions.ATTACK_COMPONENT.validate(outOfRangeAction, this.game);
+
+          expect(result.isValid).to.be.false;
+          expect(result.error).to.contain('Bob is out of Knife\'s attack range!');
+        });
+      });
+      context('when the gun shot is not in a straight line', function () {
+        beforeEach(function () {
+          this.p2.character.row = 0;
+          this.p2.character.col = 0;
+        });
+        afterEach(function () {
+          this.p2.character.row = 1;
+          this.p2.character.col = 1;
+        });
+        it('should not be valid', function () {
+          const diagonalShotAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            targetName: 'bob',
+            times: 3,
+            weaponName: 'Rifle',
+            timestamp: Date.now(),
+            key: 'Attack'
+          };
+
+          const result = Actions.ATTACK_COMPONENT.validate(diagonalShotAction, this.game);
+
+          expect(result.isValid).to.be.false;
+          expect(result.error).to.contain('You can only shoot in straight lines!');
+        });
+      });
+      context('when the gun shot is obstructed by a barrier', function () {
+        beforeEach(function () {
+          this.p2.character.col = 3;
+        });
+        afterEach(function () {
+          this.p2.character.col = 1;
+        });
+        it('should not be valid', function () {
+          const obstructedShotAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            targetName: 'b',
+            times: 2,
+            weaponName: 'Rifle',
+            timestamp: Date.now(),
+            key: 'Attack'
+          };
+
+          const result = Actions.ATTACK_COMPONENT.validate(obstructedShotAction, this.game);
+
+          expect(result.isValid).to.be.false;
+          expect(result.error).to.contain('Your shot is obstructed by a barrier!');
+        });
+      });
+      context('when the attacker doesn\'t have the required bullets', function () {
+        beforeEach(function () {
+          this.origInventory = _.cloneDeep(this.p1.character.inventory);
+
+          this.p1.character.inventory = Inventory.addToInventory(this.p1.character.inventory, 'Revolver', 1, 1);
+        });
+        afterEach(function () {
+          this.p1.character.inventory = this.origInventory;
+        });
+        it('should not be valid', function () {
+          const missingBulletsAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            targetName: 'bob',
+            weaponName: 'Revolver',
+            times: 1,
+            timestamp: Date.now(),
+            key: 'Attack'
+          };
+
+          const result = Actions.ATTACK_COMPONENT.validate(missingBulletsAction, this.game);
+
+          expect(result.isValid).to.be.false;
+          expect(result.error).to.contain('You don\'t have Revolver Bullets for your Revolver!');
+        });
+      });
+      context('when the attacker doesn\'t have enough bullets', function () {
+        it('should not be valid', function () {
+          const notEnoughBulletsAction: Actions.AttackAction = {
+            actor: this.p1.character,
+            times: 20,
+            weaponName: 'Rifle',
+            key: 'Attack',
+            timestamp: Date.now(),
+            targetName: 'Bob'
+          };
+
+          const result = Actions.ATTACK_COMPONENT.validate(notEnoughBulletsAction, this.game);
+
+          expect(result.isValid).to.be.false;
+          expect(result.error).to.contain('You cannot shoot 20 time(s) because you only have 10 bullet(s)!');
+        });
+      });
+    });
+  });
 });
